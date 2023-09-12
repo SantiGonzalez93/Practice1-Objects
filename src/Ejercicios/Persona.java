36d1e1b1f4d25a8de03d884a5db715085ed067b4
Gonzalez <sgonzalez@dgrred.atm.mendoza.gov.ar>
2023-09-12 12:55:25 -0300

    Exercise number two completed

---
+++ src/Ejercicios/Persona.java
@@ -0,0 +1,94 @@
+package Ejercicios;
+import java.util.Random;
+
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private int DNI;
+    private double peso;
+    private double altura;
+
+    public Persona() {
+        this.DNI = generaDNI();
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = generaDNI();
+    }
+
+    public Persona(String nombre, int edad, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = generaDNI();
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int calcularIMC(double peso, double altura){
+        // si la persona esta en su peso ideal
+        double valor = peso/((altura)*(altura));
+        int comparador = 0;
+        if (valor < 20) {
+            comparador = -1;
+        } else if (valor >= 20  && 25 >= valor) {
+            comparador = 0;
+        } else if ( 25 > valor) {
+            comparador = 1;
+        }
+        return comparador;
+    }
+    public boolean esMayorDeEdad (int edad) {
+        boolean result = false;
+        if (edad >= 18) {
+            result = true;
+        }
+        return result;
+    }
+
+    private int generaDNI(){
+
+        Random nroAleatorio = new Random();
+        return nroAleatorio.nextInt(100000000);
+        
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Ejercicios.Cuenta;
+import Ejercicios.Persona;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Ejercicio nro 1
         Cuenta cuenta1 = new Cuenta("Santiago", 155493.4 );
 
         cuenta1.ingresar(7, cuenta1.getCantidad());
@@ -11,6 +13,22 @@ public class Main {
 
         cuenta1.retirar(5000, cuenta1.getCantidad());
         System.out.println("la nueva cantidad actual es " + cuenta1.getCantidad());
-    }
 
+        // Ejercicio nro 2
+
+        Persona santi = new Persona("Santiago", 30, 70.1, 1.73);
+
+        int resuesta = santi.calcularIMC(santi.getPeso(), santi.getAltura());
+
+        System.out.println("el indice de masa corporal es: "+resuesta);
+
+        System.out.println("la persona es mayor de edad? "+santi.esMayorDeEdad(santi.getEdad()));
+
+        System.out.println("el DNI es: " + santi.getDNI());
+
+        //Ejercicio nro 3
+
+
+
+    }
 }
